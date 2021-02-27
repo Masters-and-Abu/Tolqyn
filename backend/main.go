b3213c0be4d625f0d31b3e49521547e6ebba6986
Abunyawa <abusaid.manap@gmail.com>
2021-02-27 17:47:35 +0600

    feat: TOL-7 asd
    asd

--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"github.com/Masters-and-Abu/Tolqyn/backend/auth"
 	"github.com/Masters-and-Abu/Tolqyn/backend/database"
+	"github.com/Masters-and-Abu/Tolqyn/backend/broadcast"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/prometheus/common/log"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/net/context"
 	"net/http"
 	"os"
@@ -42,31 +41,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/register", auth.Register)
 	r.HandleFunc("/auth", auth.Auth)
+	r.HandleFunc("/sdp", broadcast.SDP)
 
 	fmt.Println("Listening on port " + port)
 	http.ListenAndServe(":"+port, r)
-}
-
-func getList(writer http.ResponseWriter, request *http.Request) {
-
-	users := database.DB.Client.Database("tolqyn").Collection("users")
-
-	findOptions := options.Find()
-	cur, err := users.Find(context.TODO(), bson.D{{}}, findOptions)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	for cur.Next(context.TODO()) {
-		if cur == nil {
-			break
-		}
-		usr := auth.User{}
-		err := cur.Decode(&usr)
-		if err != nil {
-			fmt.Println("problem in decode")
-		}
-
-		writer.Write([]byte(usr.Login + "\n"))
-	}
 }
